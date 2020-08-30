a28335fe5701cbeb83a38b3093c5a5028d791bf0
Jos de Jong <wjosdejong@gmail.com>
2020-08-30 14:11:24 +0200

    Add eslint to the project

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    mocha: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
