3f9a7def751fe0d505354f6897ac679891f2510e
Jos de Jong <wjosdejong@gmail.com>
2020-09-30 11:59:21 +0200

    throttle search progress again

--- src/components/treemode/TreeMode.svelte
+++ src/components/treemode/TreeMode.svelte
@@ -12,7 +12,8 @@
     STATE_EXPANDED,
     STATE_LIMIT,
     SCROLL_DURATION,
-    SIMPLE_MODAL_OPTIONS
+    SIMPLE_MODAL_OPTIONS,
+    SEARCH_PROGRESS_THROTTLE
   } from '../../constants.js'
   import { createHistory } from '../../logic/history.js'
   import JSONNode from './JSONNode.svelte'
@@ -32,7 +33,7 @@ findRootPath
   import { keyComboFromEvent } from '../../utils/keyBindings.js'
   import { searchAsync, searchNext, searchPrevious, updateSearchResult } from '../../logic/search.js'
   import { immutableJSONPatch } from '../../utils/immutableJSONPatch'
-  import { last, initial, cloneDeep, uniqueId } from 'lodash-es'
+  import { last, initial, cloneDeep, uniqueId, throttle } from 'lodash-es'
   import jump from '../../assets/jump.js/src/jump.js'
   import { expandPath, syncState, patchProps } from '../../logic/documentState.js'
   import Menu from './Menu.svelte'
@@ -79,6 +80,8 @@ findRootPath
     searchResult = updateSearchResult(doc, results, searchResult)
   }
 
+  const handleSearchProgressDebounced = throttle(handleSearchProgress, SEARCH_PROGRESS_THROTTLE)
+  
   function handleSearchDone (results) {
     searchResult = updateSearchResult(doc, results, searchResult)
     searching = false
@@ -120,7 +123,7 @@ findRootPath
     searching = true
 
     searchHandler = searchAsync(searchText, doc, {
-      onProgress: handleSearchProgress,
+      onProgress: handleSearchProgressDebounced,
       onDone: handleSearchDone
     })
   }
